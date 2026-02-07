2dce20b8105f29517f3a38b3fc46e9fe1b38a262
vimalmahalingam <vimalmahalingam.m@gmail.com>
2026-02-07 19:57:47 +0530

    Add files via upload

---
+++ SOnarqube.java
@@ -0,0 +1,13 @@
+# Project identification
+sonar.projectKey=my_project
+sonar.projectName=My Project
+sonar.projectVersion=1.0
+
+# Source code location
+sonar.sources=src
+
+# Language (optional, auto-detected)
+sonar.language=java
+
+# Encoding
+sonar.sourceEncoding=UTF-8
